1dcccf8a8b0ceea852e7db1bea85b1fc15c1b5d7
Tung Pham <tungpham.bh@gmail.com>
2019-11-19 05:40:28 +0700

    update lesson 21

--- react-crud/src/App.js
+++ react-crud/src/App.js
@@ -2,9 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todos:[{
+        id: 1, name:"Học ReactJS"
+      },{
+        id: 2, name:"Kiếm Tiền"
+      },{
+        id: 3, name: "Mua Sữa Cho Nhím"
+      }]
+    };
+  }
+
+  render(){
+    return <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,8 +32,15 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <h2 className="text-center">Todo App</h2>
+      <ul className="list-group text-center">
+        {this.state.todos.map((item) => {
+        return <li className="list-group-item" id="item.id">{item.name}</li>
+        })}
+      </ul>
     </div>
-  );
+  }
 }
 
 export default App;
